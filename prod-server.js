8b22d3ae6a1fbf7cba4887a27a54fc63c5f3bc31
chfengmtcs <1036396370@qq.com>
2022-04-26 11:22:17 +0800

    fix: dotenv

--- prod-server.js
+++ prod-server.js
@@ -1,5 +1,5 @@
 // prod-server.js
-require('dotenv').config(); // require dotenv
+import 'dotenv/config';
 const cli = require('next/dist/cli/next-start');
 
 cli.nextStart(['-p', process.env.PORT || 3000]);

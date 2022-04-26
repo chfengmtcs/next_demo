390070f499c52d9015f5ad69eae0e4fd8e95310a
chfengmtcs <1036396370@qq.com>
2022-04-26 11:49:40 +0800

    fix: add port

--- dev-server.ts
+++
@@ -1,5 +0,0 @@
-// dev-server.js
-import 'dotenv/config';
-import cli from 'next/dist/cli/next-dev';
-
-cli.nextDev(['-p', process.env.PORT || '3000']);
--- prod-server.ts
+++
@@ -1,5 +0,0 @@
-// prod-server.js
-import 'dotenv/config';
-import cli from 'next/dist/cli/next-start';
-
-cli.nextStart(['-p', process.env.PORT || '3000']);
